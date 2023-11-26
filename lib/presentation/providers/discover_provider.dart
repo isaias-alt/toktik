6a8d5723acf120ccff9dfc5fef708171b9d46830
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-11-25 21:02:37 -0300

    add video player

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -9,7 +9,6 @@ class DiscoverProvider extends ChangeNotifier {
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    await Future.delayed(const Duration(seconds: 1));
     final List<VideoPost> newVideos = videoPosts
         .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
         .toList();
--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -17,6 +18,10 @@ class VideoScrollableView extends StatelessWidget {
         final VideoPost videoPost = videos[index];
         return Stack(
           children: [
+            SizedBox.expand(
+              child: FullScreenPlayer(caption: videoPost.caption, videoUrl: videoPost.videoUrl),
+            ),
+
             Positioned(
               bottom: 40,
               right: 20,
