b12ff196b014fc2cf508b12fdcd931bcab39a1cc
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-11-28 13:07:09 -0300

    Add Datasource & Repository

---
+++ lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:toktik/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
+class VideoPostsRepositoryImpl implements VideoPostRepository {
+  final VideoPostsDatasource videosDatasource;
+
+  VideoPostsRepositoryImpl({required this.videosDatasource});
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userId) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosDatasource.getTrendingVideosByPage(page);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screen/discover/discover_screen.dart';
 
@@ -12,19 +14,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository =
+        VideoPostsRepositoryImpl(videosDatasource: LocalVideoDatasource());
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage()
-        )
+            lazy: false,
+            create: (_) =>
+                DiscoverProvider(videosRepository: videoPostRepository)
+                  ..loadNextPage())
       ],
       child: MaterialApp(
-        title: 'TokTik',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme().getTheme(),
-        home: const DiscorverScreen()
-      ),
+          title: 'TokTik',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme().getTheme(),
+          home: const DiscorverScreen()),
     );
   }
 }
